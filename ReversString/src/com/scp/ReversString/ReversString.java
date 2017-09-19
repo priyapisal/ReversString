4e8911dfef0d278b2c70d61c5ffc795f6730352f
HP <HP@192.168.2.6>
2017-09-20 00:17:44 +0530

    reversestring commit

---
+++ ReversString/src/com/scp/ReversString/ReversString.java
@@ -0,0 +1,14 @@
+package com.scp.ReversString;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class ReversString {
+	public static void main(String a[])
+	{
+		String str="Welcome";
+		char[] try1=str.toCharArray();
+		for(int i=try1.length-1;i>0;i--)
+				System.out.print(try1[i]);
+				
+			}
+	}
